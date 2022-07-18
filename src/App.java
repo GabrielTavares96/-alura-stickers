3b2e6a8451ea739ec358c8046b5b43ad0d030094
GabrielTavares96 <gabriel_sbo1996@hotmail.com>
2022-07-18 13:39:52 -0300

    First commit, primeira aula imersaoJava

---
+++ src/App.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+//        String url = "https://imdb-api.com/en/API/Top250Movies/k_12345678";
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        URI endereco = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String body = response.body();
+//        System.out.println(body);
+
+        JsonParser jsonParser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = jsonParser.parse(body);
+        System.out.println(listaDeFilmes.size());
+
+        for (Map<String, String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+
+    }
+}
